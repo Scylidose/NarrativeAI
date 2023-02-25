5658d7b9f8a5efdde150af764c7d97d32384ab51
Scylidose <jordisaleilles@hotmail.fr>
2023-02-25 16:58:56 -0500

    Added command line arguments for script.

--- generate_script.py
+++ generate_script.py
@@ -1,6 +1,8 @@
 from transformers import GPTNeoForCausalLM, GPT2Tokenizer
 import torch
 
+import argparse
+
 
 def generate_text(model_dir: str, results_dir: str,
                   prompt: list, max_length: int = 10000,
@@ -50,3 +52,41 @@ def generate_text(model_dir: str, results_dir: str,
     # Export generated text to a file
     with open(results_dir + "generated_text.txt", "w", encoding='utf-8') as f:
         f.write(generated_text)
+
+
+if __name__ == '__main__':
+
+    parser = argparse.ArgumentParser(
+        description='Generate new text based on a given prompt using a '
+                    'trained transformer model'
+    )
+
+    parser.add_argument(
+        'model_dir', type=str,
+        help='Path to the directory where the pre-trained model is stored'
+    )
+    parser.add_argument(
+        'results_dir', type=str,
+        help='Path to the directory where the generated text should be stored'
+    )
+    parser.add_argument(
+        'prompt', type=str,
+        help='The prompt to generate new text from'
+    )
+    parser.add_argument(
+        '--max_length', type=int, default=10000,
+        help='The maximum number of tokens to generate'
+    )
+    parser.add_argument(
+        '--num_sequences', type=int, default=1,
+        help='The number of independent sequences to generate for each prompt'
+    )
+    parser.add_argument(
+        '--do_sample', type=bool, default=True,
+        help='Whether to use sampling or greedy decoding to generate new text'
+    )
+
+    args = parser.parse_args()
+
+    generate_text(args.model_dir, args.results_dir, args.prompt,
+                  args.max_length, args.num_sequences, args.do_sample)
--- train_model.py
+++ train_model.py
@@ -9,6 +9,8 @@ import glob
 import random
 import numpy as np
 
+import argparse
+
 
 def load_dataset(file_path: str,
                  tokenizer: PreTrainedTokenizer) -> TextDataset:
@@ -86,8 +88,10 @@ def train_gpt_neo(train_data_dir: str, eval_data_dir: str, model_dir: str,
     Trains a GPT-Neo language model on the provided dataset.
 
     Args:
-        train_data_dir (str): The directory containing the cleaned input training data.
-        eval_data_dir (str): The directory containing the cleaned input eval data.
+        train_data_dir (str): The directory containing the cleaned input
+                              training data.
+        eval_data_dir (str): The directory containing the cleaned input
+                             eval data.
         model_dir (str): The directory to save the trained model and training
                          logs.
         model_name (str, optional): The name of the pretrained GPT-Neo
@@ -129,7 +133,7 @@ def train_gpt_neo(train_data_dir: str, eval_data_dir: str, model_dir: str,
         model=model,
         args=training_args,
         data_collator=data_collator,
-        train_dataset=train_tokenized_dataset, 
+        train_dataset=train_tokenized_dataset,
         eval_dataset=eval_tokenized_dataset
     )
 
@@ -140,3 +144,48 @@ def train_gpt_neo(train_data_dir: str, eval_data_dir: str, model_dir: str,
 
     # Save the model
     trainer.save_model(model_dir)
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(
+        description='Train a GPT-Neo model on input text data'
+    )
+
+    parser.add_argument(
+        'train_data_dir', type=str,
+        help='The directory containing the cleaned input training data.'
+    )
+    parser.add_argument(
+        'eval_data_dir', type=str,
+        help='The directory containing the cleaned input eval data.'
+    )
+    parser.add_argument(
+        'model_dir', type=str,
+        help='The directory to save the trained model and training logs.'
+    )
+    parser.add_argument(
+        '--model_name', type=str, default="EleutherAI/gpt-neo-1.3B",
+        help='The name of the pretrained GPT-Neo model to use.'
+    )
+    parser.add_argument(
+        '--batch_size', type=int, default=4,
+        help='The number of training samples per batch.'
+    )
+    parser.add_argument(
+        '--epochs', type=int, default=1,
+        help='The number of times to iterate over the training data.'
+    )
+    parser.add_argument(
+        '--learning_rate', type=float, default=2e-5,
+        help='The learning rate for the optimizer.'
+    )
+    parser.add_argument(
+        '--weight_decay', type=float, default=0.01,
+        help='The amount of weight decay to apply during training.'
+    )
+
+    args = parser.parse_args()
+
+    train_gpt_neo(args.train_data_dir, args.eval_data_dir,
+                  args.model_dir, args.model_name, args.batch_size,
+                  args.epochs, args.learning_rate, args.weight_decay)
